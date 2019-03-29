5325fb22b3d5d218ee40894e21fcc8e11ec85a88
ivangaitan <ivangaitan.kp@gmail.com>
2019-03-29 14:43:24 -0300

    commit-2

--- Apps/Votos/models.py
+++ Apps/Votos/models.py
@@ -4,3 +4,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Distrito (models.Model):
+    nom = models.CharField(max_length = 50)
+
+class Candidato (models.Model):
+    nombre = models.CharField(max_length = 20)
+    apellido = models.CharField(max_length = 50)
+    edad = models.SmallIntegerField(null=False)
+    distrito = models.ForeignKey(Distrito)
+
+class Voto (models.Model):
+    valido = models.BooleanField(default=True)
+    candidato = models.ForeignKey(Candidato)
+
+
+
+
